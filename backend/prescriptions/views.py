288e45439e808bad41cc299596b5093e14f81d14
govindpatel <govindpatel2510734@gmail.com>
2026-08-14 04:14:47 +0530

    wired both together

--- backend/prescriptions/views.py
+++ backend/prescriptions/views.py
@@ -12,7 +12,7 @@ from .serializers import (
 from .ocr_utils import extract_text_from_image
 from .qr_utils import generate_qr_code
 
-FRONTEND_BASE_URL = "http://localhost:5173"  # change this when you deploy
+FRONTEND_BASE_URL = "medical-data-extraction-ai-17s3-7awic5l7z.vercel.app"  # change this when you deploy
 
 
 class PrescriptionUploadView(generics.CreateAPIView):
