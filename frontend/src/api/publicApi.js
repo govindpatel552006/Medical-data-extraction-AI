fdab91cf247e940c0c91d2e59c42188d8d419cbf
govindpatel <govindpatel2510734@gmail.com>
2026-08-14 04:02:27 +0530

    Point frontend to live Render backend

--- frontend/src/api/publicApi.js
+++ frontend/src/api/publicApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'https://medical-data-extraction-medai.onrender.com/api';
 
 export const getPublicRecord = async (token) => {
     const response = await axios.get(`${API_BASE_URL}/prescriptions/public/${token}/`);
