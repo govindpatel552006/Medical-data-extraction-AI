1e7858f277e8630a6ede4856e97fce3f552b7d8a
govindpatel <govindpatel2510734@gmail.com>
2026-08-15 22:03:31 +0530

    Fix media file serving in production (was gated behind DEBUG)

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -26,5 +10,8 @@ urlpatterns = [
     path('api/dietplan/', include('dietplan.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files even outside DEBUG mode.
+# Not ideal for large-scale production (a CDN/S3 would be better),
+# but fine for this deployment since Render's free tier has no
+# separate static file server for media.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
